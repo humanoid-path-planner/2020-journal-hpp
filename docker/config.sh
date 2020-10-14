b221c5161d1b4debcc661d2a7d0ee86a2e8e781b
Florent Lamiraux <florent@laas.fr>
2020-10-14 14:33:01 +0200

    Add docker directory.

---
+++ docker/config.sh
@@ -0,0 +1,8 @@
+export PATH=/opt/openrobots/bin:$PATH
+export LD_LIBRARY_PATH=/opt/openrobots/lib
+export PYTHONPATH=/opt/openrobots/lib/python3.6/site-packages
+export ROS_PACKAGE_PATH=/opt/openrobots/share:/opt/ros/melodic/share
+export CMAKE_PREFIX_PATH=/opt/openrobots:/opt/ros/melodic
+export PKG_CONFIG_PATH=/opt/openrobots/lib/pkgconfig
+
+alias python='python3'
